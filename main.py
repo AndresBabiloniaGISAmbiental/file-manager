2adffe8329efc410ec9cde771585e73cdd79cde9
Andres Babilonia Torres <ababiloniatorres95@gmail.com>
2021-12-29 14:14:25 -0500

    Refactoring a funciones, pero luego al momento de ejecutar en la funcion de createFiles(), se me olvido colocar el 4 argumento que es la extension del archivo en este caso el .m

--- main.py
+++ main.py
@@ -1,36 +1,15 @@
+from managercontent import readContent
 from managerdir import createDir, readpathDir
 from mangerfiles import createFiles
+from normalize import cleanContent
 
 
-# TODO: Me gustaria implementar una funcion que tenga un temporizador si llega a cero entonces crea una carpeta por defecto
 print("UI: Iniciando programa de creación automatica, automatizada de archivos '.md' , para escribir tus notas de algun tema")
 
-# *TODO Esta parte tambien puede ser interactiva, el FICHERO txt
-def generateContent(dirpath):
-    open_file_text = open(dirpath + '\\texto.txt', 'r', encoding="utf-8")
-    content = open_file_text.readlines()
-    open_file_text.close()
-    '''Aqui se lee  lo que tiene el archivo en cada linea, y se guarda en la varibale content, que se convierte En una lista o array donde cada indice tiene una linea de texto y/o un salto de linea.'''
-    '''Abre el block de notas y guarda el contenido en la variable content, en memoria en pocas palabras, para luego en este bucle, infinito o indeterminado, al ingresar en cada linea'''
-    """ Delete line with cero or  empyty, empity whatever"""
-    # open(path+"\\texto2.txt", 'w')
-    i = len(content)-1
-    '''Establezco el contado, no se porque le quito 1, creo que es por ensayo y error..empiezo por la ultima linea'''
-    while  True:
-        try:
-            if content[i] != '\n' and i <= len(content):
-                i = i-1
-            else:
-                content.pop(i)
-        except:
-            break
-    ''' Borro el elemento en la posicion que me indica que esta  el caracter de salto de linea. Busco la coincidencia el salto de linea.
-    limpieza del archivo de saltos de lineas, para dejar solo el texto util.'''
-    #TODO Refactorizar el while por un for si se quiere.
-    return content
 
-
+file = 'texto.txt'
 dirpath = readpathDir()
-folder = createDir()
-content = generateContent(dirpath)
-createFiles(content, folder, dirpath)
+folder = createDir('aa')
+content = cleanContent(readContent(dirpath, file))
+
+createFiles(content, folder, dirpath, 'md')
